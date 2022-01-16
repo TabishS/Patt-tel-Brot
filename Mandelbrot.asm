88e2f8a78a85d520e686b7d2e923b7a4c45be42e
Tabish Shaik <tfshaik2@illinois.edu>
2022-01-15 18:45:32 -0600

    Added intermediate messages

--- Mandelbrot.asm
+++ Mandelbrot.asm
@@ -40,7 +40,7 @@ WIDTHFOUND
     BR SIZECHECK
 HEIGHTFOUND
     ST R3, HEIGHT
-    HALT
+    BR PREBROTCALC
 ; Width and Height checks are the same, but one uses newlines
 
 FIRSTCHAR
@@ -53,11 +53,6 @@ NEWLINE
     .FILL x000A
 NEGNEWLINE
     .FILL xFFF5
-
-WIDTH
-    .BLKW #1
-HEIGHT
-    .BLKW #1
 
 DIMCHECKMESSAGE ; Change to backspace?
     ; "Press space until the end of your console is reached, then hit enter. To restart, press delete\n\n"
@@ -111,13 +106,160 @@ DIMCHECKMESSAGE ; Change to backspace?
     .FILL x0A0A
     .FILL x0000
 
-M_XA
-    .FILL x0278 ; 2.47
-M_XB
-    .FILL x013C ; 1.235
-M_YA
-    .FILL x023D ; 2.24
-M_YB
-    .FILL x011E ; 1.12
+PREBROTCALC
+    LEA R0, #2
+    PUTSP
+    BR BROTCALC
+    ; "Use w, a, s, d to pan, i and o to zoom in and out, and j and k to increase or decrease the maximum number of iterations"
+    .FILL x7355
+    .FILL x2065
+    .FILL x2C77
+    .FILL x6120
+    .FILL x202C
+    .FILL x2C73
+    .FILL x6420
+    .FILL x7420
+    .FILL x206F
+    .FILL x6170
+    .FILL x2C6E
+    .FILL x6920
+    .FILL x6120
+    .FILL x646E
+    .FILL x6F20
+    .FILL x7420
+    .FILL x206F
+    .FILL x6F7A
+    .FILL x6D6F
+    .FILL x6920
+    .FILL x206E
+    .FILL x6E61
+    .FILL x2064
+    .FILL x756F
+    .FILL x2C74
+    .FILL x6120
+    .FILL x646E
+    .FILL x6A20
+    .FILL x6120
+    .FILL x646E
+    .FILL x6B20
+    .FILL x7420
+    .FILL x206F
+    .FILL x6E69
+    .FILL x7263
+    .FILL x6165
+    .FILL x6573
+    .FILL x6F20
+    .FILL x2072
+    .FILL x6564
+    .FILL x7263
+    .FILL x6165
+    .FILL x6573
+    .FILL x7420
+    .FILL x6568
+    .FILL x6D20
+    .FILL x7861
+    .FILL x6D69
+    .FILL x6D75
+    .FILL x6E20
+    .FILL x6D75
+    .FILL x6562
+    .FILL x2072
+    .FILL x666F
+    .FILL x6920
+    .FILL x6574
+    .FILL x6172
+    .FILL x6974
+    .FILL x6E6F
+    .FILL x2E73
+    .FILL x5020
+    .FILL x6572
+    .FILL x7373
+    .FILL x6520
+    .FILL x746E
+    .FILL x7265
+    .FILL x7420
+    .FILL x206F
+    .FILL x6562
+    .FILL x6967
+    .FILL x2C6E
+    .FILL x6120
+    .FILL x646E
+    .FILL x7020
+    .FILL x6572
+    .FILL x7373
+    .FILL x6420
+    .FILL x6C65
+    .FILL x7465
+    .FILL x2065
+    .FILL x6F74
+    .FILL x7220
+    .FILL x7365
+    .FILL x7A69
+    .FILL x2065
+    .FILL x6F79
+    .FILL x7275
+    .FILL x7720
+    .FILL x6E69
+    .FILL x6F64
+    .FILL x0077
+
+BROTCALC
+    HALT
+
+SCALEWIDTH
+    .FILL x0004
+    .FILL x8000
+WIDTH
+    .BLKW #1
+HEIGHT
+    .BLKW #1
+SMALLERDIMINT
+    .BLKW #1
+SMALLDERDIMFRAC
+    .BLKW #1
+ISHIFTINT
+    .BLKW #1
+ISHIFTFRAC
+    .BLKW #1
+JSHIFTINT
+    .BLKW #1
+JSHIFTFRAC
+    .BLKW #1
+XSHIFT
+    .BLKW #1
+YSHIFT
+    .BLKW #1
+ZOOM
+    .BLKW #1
+MI
+    .BLKW #1
+
+; UPKEY 
+;     .FILL x0077
+; DOWNKEY
+;     .FILL x0073
+; ZOOMOUTKEY
+;     .FILL x006F
+; MIMINUSKEY
+;     .FILL x006B
+; MIPLUSKEY
+;     .FILL x006A
+; ZOOMINKEY
+;     .FILL x0069
+; RIGHTKEY
+;     .FILL x0064
+; LEFTKEY
+;     .FILL x0061
+NEGLEFTKEY
+    .FILL xFF9E
+
+; R0 - I/O, temporary values
+; R1 - i
+; R2 - j
+; R3 - 
+; R4 - 
+; R5 - 
+; R6 - 
+; R7 - JSR, temporary values
 
 .END
